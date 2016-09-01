b7764e3107a39e5edaa6fdb29476e0022e9446da
Winnie Luk <winniecluk@gmail.com>
2016-09-01 09:24:17 -0700

    review checklist

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 var counter = 0;
 var score = 0;
 var timer = 30;
-// var won = false;
 
 // TIMER FUNCTION
 
@@ -14,21 +13,14 @@ function startTimer(){
   stopGame = setTimeout(function(){
     stop();
   }, 31000);
+  // 5 seconds just for testing
   // }, 5000);
   timerCountdown = setInterval(function(){
     $('#timer').html('Timer: ' + timer--);
   }, 1000);
 }
 
-//     stop();
-//   }, 61000);
-//   // }, 5000);
-//   setInterval(function(){
-//     $('#timer').html('Timer: ' + timer--);
-//   }, 1000);
-// }
-
-// VIEW
+// SCREEN DISPLAY
 
 function showScreen(id){
   $(id).show();
@@ -46,24 +38,21 @@ function changeScoreDisplay(){
 
 // STOP GAME
 function stop(){
-  console.log(stopGame);
   clearTimeout(stopGame);
-  console.log(timerCountdown);
   clearInterval(timerCountdown);
-  console.log(regenerateID);
   clearInterval(regenerateID);
   if (score < 26) {
-    window.alert('you lost!');
+    window.alert('Time is up!');
     $('#game').hide();
     $('#lose').show();
   } else {
-    window.alert('you won!');
+    window.alert('Time is up!');
     $('#game').hide();
     $('#win').show();
   }
 }
 
-// DIV CONTROLLER
+// DIV NAVIGATION
 
 function init() {
   $('#splash').show();
@@ -83,58 +72,36 @@ function init() {
     hideScreen('#instructions');
     showScreen('#game');
     startTimer();
-    // makeNewSpiders();
-    // deploySpiders();
     replaceSpiders();
-    // addClass();
   });
   $('.replay-yes').on('click', function(evt){
     hideScreen('#win');
     hideScreen('#lose');
     showScreen('#game');
     startTimer();
-    // clearInterval(replaceSpiders);
-    // replaceSpiders();
     score = 0;
     changeScoreDisplay();
     replaceSpiders();
-    // won = false;
     timer = 30;
   });
 }
 
-// CONTROLLER
+
 // up score when correct spider is hit
 function correctHit(){
   score++;
-  console.log(score);
 }
 
 // down score when wrong spider is hit
 function wrongHit(){
   if (score > 0){
     score--;
-    console.log(score);
   } else {
     return;
   }
 }
 
-// function checkWinner(){
-//   if (score >= 8){
-//     $('#game').hide();
-//     window.alert('you won!');
-//     showScreen('#win');
-//     won = true;
-//   }
-// }
-
-// to deploy spiders/ get spiders to come back
-// var $newSpiders = $('<img class="true" src="images/tinydomestichouse.png">');
-// var $badSpiders = $('<img class="false" src="images/tinyhobo.png">');
-// var $spiderColumn = $('.spider-column');
-
-// get random number
+// get random numbers
 function getRandom(){
   return Math.round(Math.random());
 }
@@ -143,56 +110,10 @@ function getRandom3(){
   return Math.floor(Math.random() * 3);
 }
 
-// deploy randomspiders
-// $('.spider-column').each(function(ind, column){
-//   if ($(column).html().trim() == ""){
-//     $(this).html('<img class="true" src="images/tinydomestichouse.png">');
-//   }
-
-// function makeNewSpiders (){
-//   $('.spider-column').each(function(){
-//     var result = getRandom();
-//     if(result == 1){
-//       $(this).append('<img class="true" src="images/tinydomestichouse.png">');
-//     } else {
-//       $(this).append('<img class="false" src="images/tinyhobo.png">');
-//     }
-//     console.log('makeNewSpiders works');
-//   });
-// }
-
-//   $spiderColumn.append($newSpiders);
-//   // when you deploy again, need to turn class back into .spider-column
-//   // $('.empty').addClass('.spider-column').removeClass('.empty');
-//   console.log('makeNewSpiders works');
-// }
-
-
-// deploySpiders -- if column is empty after trimming white space
-// append good or bad spider at random
-// unless good spiders < 3
-
-// function deploySpiders(){
-//   setInterval (function() {
-//     console.log('deploySpiders works');
-//     $('.spider-column').each(function(ind, column){
-//       if ($(column).html().trim() == ""){
-//         var result = getRandom();
-//         if($('.true').length < 2){
-//           $(this).append('<img class="true spider" src="images/tinydomestichouse.png">');
-//         } else if (result == 1){
-//           $(this).append('<img class="true spider" src="images/tinydomestichouse.png">');
-//         } else {
-//           $(this).append('<img class="false spider" src="images/tinyhobo.png">');
-//         }
-//         // $(this).html('<img class="true" src="images/tinydomestichouse.png">');
-//       }
-//     });f
-//   }, 5000);
-// }
-
+// ID for setInterval so I can clear it
 var regenerateID;
 
+//
 function replaceSpiders(){
   regenerateID = setInterval(function(){
     $('.spider-column').each(function(ind, column){
@@ -205,42 +126,13 @@ function replaceSpiders(){
         $(this).append('<img class="false spider p' + resultID + '" src="images/jumping-spider.png">');
       }
     });
-  }, 6000);
+  }, 4000);
 }
 
-//
-
-// function checkForEmptycolumn(){
-//   $('.spider-column').each(function(ind, column){
-//     // debugger;
-//     if ($(column).html().trim() == ""){
-//       console.log('hello!');
-//     }
-//   });
-// }
-
-// function addClass(){
-//   $('img').addClass('true');
-//   console.log('addClass works');
-// }
-
-    // $('td').filter('.empty').html('<img class="true" src="images/tinydomestichouse.png">');
-    // $('td').addClass('.spider-column').removeClass('.empty');
-
-//     setInterval(makeNewSpiders, 5000);
-//   }
-// }
-
-//$('.col').html();
-
-// find out which ones don't have a spider
-// append a new spider to it
-
-// to check whether or not the spider is correct
-
+// variable to grab gameboard
 var $gameboard = $('#gameboard');
 
-// $('.true').on('click', checkWinner);
+// when you remove wrong spiders
 $('document').ready(function(){
   $gameboard.on('click', '.false', function() {
     $(this).remove();
@@ -248,8 +140,6 @@ $('document').ready(function(){
     changeScoreDisplay();
   });
 });
-
-// $('.gameboard').on('click', '.true', correctHit);
 
 // to remove spiders who were hit
 $('document').ready(function(){
@@ -260,37 +150,3 @@ $('document').ready(function(){
     changeScoreDisplay();
   });
 });
-// change class of 'td' to empty
-// $gameboard.on('click', '.true', function(){
-//   $(this).closest('td').removeClass('spider-column').addClass('empty');
-//   console.log('empty now');
-// });
-
-
-// CANVAS
-// get canvas and make drawing surface
-// var canvas = document.querySelector('.canvas-class');
-// var context = canvas.getContext('2d');
-// var imageSpiderTrue = new Image();
-
-// imageSpiderTrue.onload = function() {
-//   context.drawImage(imageSpiderTrue, 150, 150);
-// };
-
-// imageSpiderTrue.src = 'images/tinydomestichouse.png';
-
-// context.font = '40pt Calibri';
-// context.fillText('Hello World!', 150, 100);
-
-// context.translate(canvas.width / 2, canvas.height / 2);
-
-
-
-//1st goal
-// look over stuff we did and duplicate it
-// 2nd goal
-// put some images into screen
-// 3rd goal: learn canvas
-// html5canvastutorials.com
-// 4th goal
-//
